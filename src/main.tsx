a220f05d3c5d029af675c5acec90d5761a6e60b4
Dax Harris <mvh3875@rit.edu>
2024-08-16 13:22:43 -0400

    switch to RC

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+
 import "@mantine/core/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
